9cf33847a7b8f9bba6d708d9d8b3721f9f785832
Tom <skatereal@hotmail.com>
2017-04-10 10:48:31 -0800

    Collatz in go

---
+++ Collatz.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func collatz(num int) int{
+	fmt.Print(num, ", ")
+	if num == 1 {
+		return 1
+	}else if num % 2 == 0 {
+		return (num / 2)
+	}else {
+		return(3 * num + 1)
+	}
+}
+
+func recursiveCollatz(num int){
+	fmt.Print(num, ", ")
+	if num == 1 {
+		return
+	}else if num % 2 == 0 {
+		collatz(num / 2)
+	}else {
+		collatz(3 * num + 1)
+	}
+}
+
+func main() {
+	num := 3
+	for num >= 1 {
+		num = collatz(num)
+		if num == 1 {
+			fmt.Print(num)
+			break
+		}
+	}
+
+	//recursiveCollatz(3)
+}
